a940b15bac85c0febb62a0cf39ffddc00b12e7ff
planetfaller <planetburning@gmail.com>
2017-05-10 22:36:26 +0200

    Added serial simulator

---
+++ cleanBuildSerial/arduino-serial-sim/arduino-serial-sim.ino
@@ -0,0 +1,16 @@
+int delayMS=1; // delay in ms
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // serial simulator, matches v1@v2@v3,
+  
+  for(int i=0; i < 360; i++){
+    String toPrint = String(random(100,400), DEC) + "@" + String(i, DEC) + "@" + delayMS*1000 + ",";// 
+  Serial.print(toPrint);
+  delay(delayMS);
+  }
+}
+
